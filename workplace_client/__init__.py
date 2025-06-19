943f96c7b047ac1af8c6a7587a0ee928c8696135
willymwai <willymwaid92@gmail.com>
2025-06-19 07:39:11 +0300

    Bump package version to 1.0.11 in configuration file

--- workplace_client/__init__.py
+++ workplace_client/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 
 # import apis into sdk package
 from workplace_client.api.available_packages_api import AvailablePackagesApi

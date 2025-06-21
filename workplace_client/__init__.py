340d41dbd398b42831e50975b8d37e51b2e84836
Patience Igiraneza <patienceigiraneza2@gmail.com>
2025-06-21 06:28:04 +0200

    Release version 1.0.13

--- workplace_client/__init__.py
+++ workplace_client/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.0.12"
+__version__ = "1.0.13"
 
 # import apis into sdk package
 from workplace_client.api.available_packages_api import AvailablePackagesApi
